d0d9ff54d0db1cd1975a3d0f8417c29da67c0d4a
Yael Castro <yy.lgnd@gmail.com>
2021-02-18 17:05:52 -0600

    update

--- sorter/sorter.go
+++ sorter/sorter.go
@@ -6,17 +6,26 @@ import (
 
 //New returns an implementation of model.Sorter
 func New() model.Sorter {
-	return quickSorter{} 
+	return quickSorter{}
 }
 
 type quickSorter struct {}
 
 func (q quickSorter) Sort(numbers *model.Numbers) {
 	numbers.Sorted = make([]int, len(numbers.Unsorted))
-
 	copy(numbers.Sorted, numbers.Unsorted)
 
 	q.sort(numbers.Sorted, 0, len(numbers.Sorted) - 1)
+
+	x := 1
+	for i := 0; i < len(numbers.Sorted) - x; i++ {
+		if numbers.Sorted[i] == numbers.Sorted[i + 1] {
+			tmp := numbers.Sorted[i]
+			numbers.Sorted = append(numbers.Sorted[:i], numbers.Sorted[i + 1:]...)
+			numbers.Sorted = append(numbers.Sorted, tmp)
+			x++
+		}
+	}
 }
 
 func (q quickSorter) sort(arr []int, start, end int) {
@@ -41,6 +50,6 @@ func (q quickSorter) sort(arr []int, start, end int) {
 	arr[end] = arr[splitIndex]
 	arr[splitIndex] = pivot
 
-	q.sort(arr, start, splitIndex-1)
-	q.sort(arr, splitIndex+1, end)
+	q.sort(arr, start, splitIndex - 1)
+	q.sort(arr, splitIndex + 1, end)
 }
--- sorter/sorter_test.go
+++ sorter/sorter_test.go
@@ -21,8 +21,8 @@ var (
 	sorter = New()
 	table = []struct{model.Numbers; expected []int} {
 		{
-			model.Numbers{Unsorted: []int{-2, 3, 4, 6, 21, 20}},
-			[]int{-2, 3, 4, 6, 20, 21},
+			model.Numbers{Unsorted: []int{-2, -2, 3, 4, 6, 21, 20}},
+			[]int{-2, 3, 4, 6, 20, 21, -2},
 		},
 		{
 			model.Numbers{Unsorted: []int{2, 4, 5, -1, 6, 22, 31}},
@@ -35,7 +35,7 @@ func TestQuickSorter_Sort(t *testing.T) {
 	for i := 0; i < len(table); i++ {
 		sorter.Sort(&table[i].Numbers)
 		if !reflect.DeepEqual(table[i].expected, table[i].Sorted) {
-			t.Errorf("Expected: %v Got: %v", table[i].expected, table[i].Sorted)
+			t.Fatalf("Expected: %v Got: %v", table[i].expected, table[i].Sorted)
 		}
 		t.Log(table[i].Sorted)
 	}
